ce70dec7321d408a120198bcbf85fc5a8362bea7
BK1918kangliangyu <2689112412@qq.com>
2019-11-12 08:28:58 +0800

    初始化1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <Header/>
+     <router-view></router-view>
     <Nav v-if="$route.meta.flag" />
-    <router-view></router-view>
+   
   </div>
 </template>
 <script>
--- src/pages/house/index.vue
+++ src/pages/house/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page">
-      
+      house
     </div>
 </template>
 <script>
