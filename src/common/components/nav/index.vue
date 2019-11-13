116fef98303f62efecb1bc83c309ebc6d44568f7
BK1918kangliangyu <2689112412@qq.com>
2019-11-13 19:30:23 +0800

    新的

--- src/common/components/nav/index.vue
+++ src/common/components/nav/index.vue
@@ -1,13 +1,14 @@
 <template>
+
     <div id="nav">
         <ul>
-            <router-link tag="li"
-            v-for="item in navs"
+            <router-link
+            tag="li"
+            v-for="item in navs" 
             :key="item.id"
+            class="a"
             :to="item.path"
-            >
-            {{item.text}}
-            </router-link>
+            >{{item.text}}</router-link>
         </ul>
     </div>
 </template> 
@@ -19,35 +20,35 @@ export default {
             navs:[
                 {
                     id:0,
-                    text:"今日推荐",
+                    text:"推荐",
                     path:"/reoommend"
                 },
                 {
                     id:1,
-                    text:"最后疯抢",
+                    text:"返厂",
                     path:"/rob"
                 },
                 {
                     id:2,
-                    text:"家电",
-                    path:"/house"
+                    text:"彩妆",
+                    path:"/vip"
                 },
                 {
                     id:3,
-                    text:"生活",
+                    text:"会员",
                     path:"/life"
                 },
                 {
                     id:4,
-                    text:"唯品•奢",
-                    path:"/vip"
+                    text:"我的",
+                    path:"/house"
                 },
             ]
         }
     }
 }
 </script>
-<style lang="scss">
+<style >
     #nav{
         width: 100%;
         height:0.3rem;
